a465a5375e0e9a745e0b02532da6346baf95d496
Joseph Chenkie <josephchenjian@icloud.com>
2020-05-26 19:14:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # Configure the Microsoft Azure Provider.
 provider "azurerm" {
   version         = ">=1.34.0"
-  subscription_id = {var.subscription_id}
-  tenant_id       = {var.tenant_id}
-  client_id       = {var.client_id}
-  client_secret   = {var.client_secret}
+  subscription_id = ${var.subscription_id}
+  tenant_id       = ${var.tenant_id}
+  client_id       = ${var.client_id}
+  client_secret   = ${var.client_secret}
 }
 
 data "azurerm_subscription" "main" {
